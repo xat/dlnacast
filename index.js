4843e3b4cb362dfdad7cb8194c1ef8c35f5815ea
Manuel Rueda <manuel.rueda.un@gmail.com>
2015-08-31 22:51:12 -0300

    Add subtitle support

--- index.js
+++ index.js
@@ -16,51 +16,47 @@ var MediaRendererClient = require('upnp-mediarenderer-client');
 
 var RendererFinder = require('./libs/RendererFinder');
 
-if (!opts._.length) {
-  console.log('Usage: dlnacast [--type <mime>] [--address <tv-ip>] <file>');
+if (!opts._.length && !opts.listRenderer) {
+  console.log('Usage: dlnacast [--type <mime>] [--address <tv-ip>] [-s <file>] <file>');
+  console.log('Usage: dlnacast --listRenderer');
   process.exit();
 }
 
-var discover = function (cb) {
-  //var cli = new Client();
+var discover = function (autoStop) {
   var rf = new RendererFinder();
 
-  if (!cb) cb = noop;
-
   var clean = function () {
-    //cli.removeListener('response', onResponse);
-    //cli._stop();
     rf.stop();
     clearTimeout(to);
   };
+  var to = null;
 
-  var to = setTimeout(function () {
+  to = setTimeout(function () {
     clean();
-    cb(new Error('device not found'));
   }, 50000);
 
-  var onResponse = function (info, msg) {
-  //var onResponse = function (meta, status, machine){
-    //if (opts.address && opts.address !== machine.address) return;
-    //console.log(info);
-    //console.log(msg);
-    clean();
-    cb(null, msg.Location);
-    //cb(null, meta.LOCATION);
-  };
+  if (autoStop){
+    rf.once('found', function (argument) {
+      clean();
+    });
+  }
 
-  //cli.on('response', onResponse);
-  rf.on('found', onResponse);
-  //cli.search(" urn:schemas-upnp-org:device:MediaRenderer:1 ");
   rf.start();
+
+  return rf;
 };
 
 
-function DIDLMetadata(url, type, title) {
+function DIDLMetadata(url, type, title, subtitle) {
   var DIDL = '';
   DIDL = DIDL + '<DIDL-Lite xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sec="http://www.sec.co.kr/">';
   DIDL = DIDL + '  <item id="f-0" parentID="0" restricted="0">';
   DIDL = DIDL + '    <dc:title>' + title + '</dc:title>';
+  if (subtitle){
+    DIDL = DIDL + '    <sec:CaptionInfo sec:type="srt">' + subtitle + '</sec:CaptionInfo>';
+    DIDL = DIDL + '    <sec:CaptionInfoEx sec:type="srt">' + subtitle + '</sec:CaptionInfoEx>';
+    DIDL = DIDL + '    <res protocolInfo="http-get:*:text/srt:*">' + subtitle +'</res>';
+  }
   DIDL = DIDL + '    <upnp:class>object.item.videoItem</upnp:class>';
   DIDL = DIDL + '    <res protocolInfo="http-get:*:' + type + ':DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=01700000000000000000000000000000" sec:URIType="public">' + url +'</res>';
   DIDL = DIDL + '  </item>';
@@ -68,40 +64,18 @@ function DIDLMetadata(url, type, title) {
   return DIDL;
 }
 
-discover(function (err, loc) {
-  if (err) {
-    console.log(err);
-    process.exit();
-  }
-
-  getPort(function (err, port) {
+function StaticContent(filePath, cb, relatedUrl){
+  var stat = fs.statSync(filePath);
+  var total = stat.size;
+  var type = opts.type || mime.lookup(filePath);
+  var url = '';
+  getPort(function(err, port){
     if (err) {
       console.log(err);
       process.exit();
     }
 
-    var cli = new MediaRendererClient(loc);
     var url = 'http://' + internalIp() + ':' + port;
-    var filePath = opts._[0];
-    var stat = fs.statSync(filePath);
-    var total = stat.size;
-    var type = opts.type || mime.lookup(filePath);
-    var isPlaying = false;
-    //console.log('FIle: ' + url);
-    // Dirty hack to support MKV on my Samsung TV..
-    // I have no idea if other TV manufactures also require this..
-    if (!opts.type && type === 'video/x-matroska') {
-      type = 'video/x-mkv';
-    }
-
-    //cli.getSupportedProtocols(function(err, protocols){
-      //console.log(protocols.map(function(a){return a.contentFormat}));
-    //});
-
-    keypress(process.stdin);
-    process.stdin.setRawMode(true);
-    process.stdin.resume();
-
     var server = http.createServer(function (req, res) {
       var range = req.headers.range;
 
@@ -109,6 +83,9 @@ discover(function (err, loc) {
       res.setHeader('Access-Control-Allow-Origin', '*');
       //res.setHeader('Accept-Ranges', 'bytes');
       res.setHeader('transferMode.dlna.org', 'Streaming');
+      if (relatedUrl){
+        res.setHeader('CaptionInfo.sec', relatedUrl);
+      }
       res.setHeader('contentFeatures.dlna.org', 'DLNA.ORG_PN=AVC_MP4_HP_HD_AAC;DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=01700000000000000000000000000000');
 
       if (!range) {
@@ -128,43 +105,83 @@ discover(function (err, loc) {
 
       return file.pipe(res);
     });
-
-    cli.load(url, {
-      autoplay: true,
-      contentType: type,
-      metadata: DIDLMetadata(url, type, path.basename(filePath))
-    }, function (err, result) {
-      if (err) {
-        console.log(err.message);
-        //process.exit();
-      }
-      console.log('playing: ', filePath);
-      console.log('use your space-key to toggle between play and pause');
-    });
-
-    cli.on('playing', function () {
-      isPlaying = true;
-    });
-
-    cli.on('paused', function () {
-      isPlaying = false;
-    });
-
-    process.stdin.on('keypress', function (ch, key) {
-      if (key && key.name && key.name === 'space') {
-        if (isPlaying) {
-          cli.pause();
-        } else {
-          cli.play();
-        }
-      }
-
-      if (key && key.ctrl && key.name === 'c') {
-        process.exit();
-      }
-    });
-
     server.listen(port);
+    cb(url);
   });
+}
 
-});
+
+
+if (opts.listRenderer){
+  discover(false).on('found', function(info, msg){
+    console.log('Device: ' + msg.Location);
+  });
+}else{
+  discover(true).once('found', function (info, msg) {
+    var loc = msg.Location;
+    var filePath = opts._[0];
+    var subtitlePath = opts.s;
+
+    StaticContent(subtitlePath ? subtitlePath : filePath, function(firstUrl){
+      if (subtitlePath){
+        StaticContent(filePath, function(secondUrl){
+          runDLNA(secondUrl, firstUrl);
+        }, firstUrl);
+      }else{
+        runDLNA(firstUrl);
+      }
+    });
+
+    function runDLNA(fileUrl, subUrl){
+      var cli = new MediaRendererClient(loc);
+      var stat = fs.statSync(filePath);
+      var total = stat.size;
+      var type = opts.type || mime.lookup(filePath);
+      var isPlaying = false;
+
+      if (!opts.type && type === 'video/x-matroska') {
+        type = 'video/x-mkv';
+      }
+
+      keypress(process.stdin);
+      process.stdin.setRawMode(true);
+      process.stdin.resume();
+
+      cli.load(fileUrl, {
+        autoplay: true,
+        contentType: type,
+        metadata: DIDLMetadata(fileUrl, type, path.basename(filePath), subUrl)
+      }, function (err, result) {
+        if (err) {
+          console.log(err.message);
+          //process.exit();
+        }
+        console.log('playing: ', filePath);
+        console.log('use your space-key to toggle between play and pause');
+      });
+
+      cli.on('playing', function () {
+        isPlaying = true;
+      });
+
+      cli.on('paused', function () {
+        isPlaying = false;
+      });
+
+      process.stdin.on('keypress', function (ch, key) {
+        if (key && key.name && key.name === 'space') {
+          if (isPlaying) {
+            cli.pause();
+          } else {
+            cli.play();
+          }
+        }
+
+        if (key && key.ctrl && key.name === 'c') {
+          process.exit();
+        }
+      });
+    }
+
+  });
+}
